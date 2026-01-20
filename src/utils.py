2d79352ad5fb87268079fc2105862ccb566c3463
MohammedTabarakAhmed <tabarakahmed030@gmail.com>
2026-01-20 23:43:06 +0530

    hyperparamter tuning for the models

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os,sys,dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -17,14 +18,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            # This is called dictionary unpacking.
+            # It takes a dictionary and expands it into keyword arguments.
             model.fit(X_train,y_train)
+
 
             y_train_pred=model.predict(X_train) #check oveerfitting
             y_test_pred=model.predict(X_test) #check performance of model
